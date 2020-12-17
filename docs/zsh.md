4c922601d1ebcca194205c1ac34ac51c69ccb4ee
Pau Tena Vidal <pau.tena@paack.co>
2020-12-17 17:57:51 +0100

    add osx apps script

--- docs/zsh.md
+++ docs/zsh.md
@@ -1,5 +1,9 @@
 # Zsh
 
+# Installation
+
+[Instructions](https://github.com/ohmyzsh/ohmyzsh/wiki/Installing-ZSH)
+
 # Font
 
 **Source code font** is the used font. You can download from [here](https://github.com/adobe-fonts/source-code-pro)
@@ -8,3 +12,7 @@
 
 - [zsh-autosuggestions](https://github.com/zsh-users/zsh-autosuggestions)
 - [zsh-syntax-highlighting](https://github.com/zsh-users/zsh-syntax-highlighting)
+
+#### Installation
+
+1. Clone the plugin repository into `$ZSH_CUSTOM/plugins` (by default `~/.oh-my-zsh/custom/plugins`)
---
+++ osx/install_apps.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Install brew and brew cask apps                                             #
+###############################################################################
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Add older versions cask repository because of 1Password subscription based business model change from v6 to v7
+
+brew tap homebrew/cask-versions homebrew/cask-fonts
+
+declare -a brew_cask_apps=(
+  'alacritty'
+  'android-file-transter'
+  'android-studio'
+  'docker'
+  'java'
+  'keybase'
+  'postman'
+  'slack'
+  'spotify'
+  'visual-studio-code'
+)
+
+for app in "${brew_cask_apps[@]}"; do
+  brew cask install "$app"
+done
+
+declare -a brew_cli_tools=(
+  'cmake'
+  'coreutils'
+  'ctags'
+  'ffmpeg'
+  'git'
+  'htop'
+  'neovim'
+  'node'
+  'nvm'
+  'tig'
+  'tmux'
+  'watchman'
+  'yarn'
+  'zsh'
+  'zsh-autosuggestions'
+  'zsh-syntax-highlighting'
+)
+
+for tool in "${brew_cli_tools[@]}"; do
+  brew install "$tool"
+done
+
+###############################################################################
+# Install Mac App Store apps                                                  #
+###############################################################################
+
+declare -a mas_apps=(
+  '497799835' # Xcode
+)
+
+for app in "${mas_apps[@]}"; do
+  mas install "$app"
+done
+
+###############################################################################
+# Configure installed apps                                                    #
+###############################################################################
+
+# Set ZSH as the default shell
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
