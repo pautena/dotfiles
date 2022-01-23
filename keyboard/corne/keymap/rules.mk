906e4c7395fadc47673d98ebdd32699771d77af9
Pau Tena <me@protonmail.com>
2022-01-23 09:44:36 +0100

    Updated keyboard setup

--- keyboard/corne/keymap/rules.mk
+++ keyboard/corne/keymap/rules.mk
@@ -1,4 +1,5 @@
-OLED_ENABLE = yes
+OLED_ENABLE = off
+RGBLIGHT_ENABLE = yes
 OLED_DRIVER = SSD1306
 # Comment this line to use an arduino miccro microprocessor.
 # The dfu bootloader is only for the Elite-C microprocessor.
