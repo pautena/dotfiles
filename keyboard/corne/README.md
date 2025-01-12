b4fe48b3e82be5d5dea7d99dc676f248bd82eb10
Pau Tena <me@protonmail.com>
2025-01-12 19:58:15 +0100

    Update corne setup

---
+++ keyboard/corne/README.md
@@ -0,0 +1,6 @@
+1. Install QMK https://docs.qmk.fm/newbs_getting_started
+2. Create or source a python venv
+3. Install qml `pip install qmk`
+4. `make setup-qmk`
+
+Ready to compile and flash
--- keyboard/corne/keymap/rules.mk
+++ keyboard/corne/keymap/rules.mk
@@ -1,6 +1,6 @@
 OLED_ENABLE = off
-RGBLIGHT_ENABLE = yes
+RGBLIGHT_ENABLE = no
 OLED_DRIVER = SSD1306
 # Comment this line to use an arduino miccro microprocessor.
 # The dfu bootloader is only for the Elite-C microprocessor.
-BOOTLOADER = dfu
+BOOTLOADER = atmel-dfu
